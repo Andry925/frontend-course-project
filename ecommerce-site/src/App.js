b5d651b506babda154d865fbfaab3ae2573f9741
Andry925 <andry925@ukr.net>
2024-11-11 12:28:10 +0200

    feat: add header and foother to app.js

--- ecommerce-site/src/App.js
+++ ecommerce-site/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap'
+
+import Header from './components/Header'
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
     <div>
-      My ecommerce app
+      <Header />
+     <main className='py-3'>
+      <Container>
+      <h1>Welcome to my technical store</h1>
+      </Container>
+  
+     </main>
+      <Footer />
     </div>
   );
 }
