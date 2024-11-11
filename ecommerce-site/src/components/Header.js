f35af7d387d048494ca23ee5a84b397dfaf6d04b
Andry925 <andry925@ukr.net>
2024-11-11 12:38:34 +0200

    feat: add icons for user and shopping cart

--- ecommerce-site/src/components/Header.js
+++ ecommerce-site/src/components/Header.js
@@ -17,8 +17,8 @@ function Header() {
                     <Navbar.Collapse id="basic-navbar-nav">
                         
                         <Nav className="mr-auto">
-                            <Nav.Link href="/cart">Cart</Nav.Link>
-                            <Nav.Link href="/login">Login</Nav.Link>
+                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                            <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
 
                             
 
