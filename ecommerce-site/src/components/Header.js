69b0ed7fcc451fd923ca2c31f7cba3482d65eb47
Andry925 <114020399+Andry925@users.noreply.github.com>
2024-11-13 13:25:40 +0200

    feat:implement react router (#4)
    * chore: install react-router-dom
    
    * feat: implement productscrem component
    
    * feat: implement react-router to header and product.
    
    * feat: add elements to app.js

--- ecommerce-site/src/components/Header.js
+++ ecommerce-site/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar, Nav, Container, Row, NavDropdown } from 'react-bootstrap'
+import { Link, LinkContainer } from 'react-router-bootstrap'
 
 
 function Header() {
@@ -9,16 +10,23 @@ function Header() {
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
+
+                    <LinkContainer to='/home'>
                     
-                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <Navbar.Brand>ProShop</Navbar.Brand>
+                    </LinkContainer>
                     
 
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         
                         <Nav className="mr-auto">
-                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
+                            <LinkContainer to='/cart'>
+                            <Nav.Link ><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer  to="/login">
+                            <Nav.Link ><i className="fas fa-user"></i>Login</Nav.Link>
+                            </LinkContainer>
 
                             
 
--- ecommerce-site/src/components/Product.js
+++ ecommerce-site/src/components/Product.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import Rating from './Rating';
+import {Link } from 'react-router-dom';
 
 function Product({ product }) {
   return (
     <Card className="my-3 p-3 rounded" style={{ width: '18rem', height: '25rem' }}>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img 
           src={product.image} 
           style={{ height: '150px', objectFit: 'cover' }} 
         />
-      </a>
+      </Link>
       <Card.Body className="d-flex flex-column justify-content-between">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div" className="text-truncate">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as="div">
           <div className="my-3">
             <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
