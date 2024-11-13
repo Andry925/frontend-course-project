be0fa5c901213ed822adbb113f1169b033a4858a
Andry925 <andry925@ukr.net>
2024-11-13 11:51:54 +0200

    feat: implement homescreenpage

--- ecommerce-site/src/screens/HomeScreen.js
+++ ecommerce-site/src/screens/HomeScreen.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import {Row, Col} from 'react-bootstrap'
-import products from '../products'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import products from "../utils/products.js";
 
 function HomeScreen() {
   return (
-    <div>
+    <Container>
       <h1>Main Products</h1>
       <Row>
         {products.map(product => (
-            <Col sm={12} md={6} lg={4} xl={3}>
-                <h3>{product.name}</h3>
-            </Col>
-
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
         ))}
       </Row>
-    </div>
-  )
+    </Container>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
