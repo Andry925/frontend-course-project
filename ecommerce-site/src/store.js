439fa132140d8cf1851dc6eec963a5cbbddd6347
Andry925 <andry925@ukr.net>
2024-12-05 21:52:11 +0200

    feat: implement store.js and products

--- ecommerce-site/src/store.js
+++ ecommerce-site/src/store.js
@@ -1,8 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {ComposeWithDevTools} from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList: productListReducer
+})
 
 const initialState = {
 
