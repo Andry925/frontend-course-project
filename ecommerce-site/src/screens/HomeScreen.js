20235feae8fbf249b31bb9d737ec2baf5b605648
Andry925 <andry925@ukr.net>
2024-12-05 22:50:40 +0200

    feat: add functionality to HomeScreen.js

--- ecommerce-site/src/screens/HomeScreen.js
+++ ecommerce-site/src/screens/HomeScreen.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
-import products from "../utils/products.js";
+import { listProducts } from '../actions/productActions.js';
 
 function HomeScreen() {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <Container>
       <h1>Main Products</h1>
-      <Row>
-        {products.map(product => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </Container>
   );
 }
