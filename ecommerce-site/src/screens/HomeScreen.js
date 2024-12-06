6212d3538ce79e2ba9907c6d9380d7e83bdcb32d
Andry925 <114020399+Andry925@users.noreply.github.com>
2024-12-06 20:39:07 +0200

    feat:implement redux state management (#6)
    * feat: implement productlistreducer
    
    * feat:implement listProducts
    
    * feat: implement store.js and products
    
    * fix: resolve dependencies error
    
    * feat: add functionality to HomeScreen.js
    
    * feat: implement productLIsts
    
    * feat: implement loader
    
    * feat: implnent productReducer
    
    * feat: implement message and Homescreen Redux

--- ecommerce-site/src/screens/HomeScreen.js
+++ ecommerce-site/src/screens/HomeScreen.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
-import products from "../utils/products.js";
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { listProducts } from '../actions/productActions.js';
 
 function HomeScreen() {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <Container>
       <h1>Main Products</h1>
-      <Row>
-        {products.map(product => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+       <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </Container>
   );
 }
