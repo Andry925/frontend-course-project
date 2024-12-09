5bd76bf717e1dfd98e747f399bc1374e8417e744
Andry925 <114020399+Andry925@users.noreply.github.com>
2024-12-09 22:09:30 +0200

    feat:add-to-cart-button (#7)
    * feat: implement productlistreducer
    
    * feat:implement listProducts
    
    * feat: implement store.js and products
    
    * fix: resolve dependencies error
    
    * feat: add functionality to HomeScreen.js
    
    * feat: implement productLIsts
    
    * feat: implement loader
    
    * feat: implnent productReducer
    
    * feat: implement message and Homescreen Redux
    
    * feat/implement-add-to-cart-button

--- ecommerce-site/src/App.js
+++ ecommerce-site/src/App.js
@@ -5,6 +5,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
           <Routes>
             <Route path='/home' element={<HomeScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart/:id' element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
