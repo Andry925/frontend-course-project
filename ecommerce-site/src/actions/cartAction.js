b94a56d33cddf47faf1f805a482ac0aa405b0176
Andry925 <114020399+Andry925@users.noreply.github.com>
2024-12-11 18:26:46 +0200

    Feat/implement add to cart functionality (#8)
    * feat: implement productlistreducer
    
    * feat:implement listProducts
    
    * feat: implement store.js and products
    
    * fix: resolve dependencies error
    
    * feat: add functionality to HomeScreen.js
    
    * feat: implement productLIsts
    
    * feat: implement loader
    
    * feat: implnent productReducer
    
    * feat: implement message and Homescreen Redux
    
    * feat/implement-add-to-cart-button
    
    * feat:implement basic actions
    
    * feat:implement CartScreen functionality
    
    * feat:implement remove from cart functionality
    
    * fix: fix stringify error

---
+++ ecommerce-site/src/actions/cartAction.js
@@ -0,0 +1,39 @@
+import {
+    CART_ADD_ITEM,
+    CART_REMOVE_ITEM,
+    CART_SAVE_SHIPPING_ADDRESS,
+    CART_SAVE_PAYMENT_METHOD,
+} from '../constants/cartConstants'
+import products from '../utils/products';
+
+
+export const addToCart = (id, qty) => (dispatch, getState) => {
+    const product = products.find((p) => p._id === id);
+
+    if (product) {
+        dispatch({
+            type: CART_ADD_ITEM,
+            payload: {
+                product: product._id,
+                name: product.name,
+                image: product.image,
+                price: product.price,
+                countInStock: product.countInStock,
+                qty,
+            },
+        });
+
+        localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+    } else {
+        console.error(`Product with id ${id} not found`);
+    }
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id,
+    });
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
